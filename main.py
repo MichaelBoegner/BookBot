375b64c6ff3b7978bb29619131cbe27a1fa46c76
michaelboegner <cmikeb@gmail.com>
2024-01-13 15:37:34 -0500

    character count report reporting unsorted

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 def main():
     book = get_book()
-    word_count = get_word_count(book)
+    # print(book)
+    word_count, words = get_word_count(book)
+    char_count = get_chars(words)
     print(f"{word_count} words found in the book.")
+    for char in char_count:
+        print(f"The {char} character was found {char_count[char]} times!")
+
 
 def get_book():
     with open("./books/frankenstein.txt") as f:
@@ -9,7 +14,16 @@ def get_book():
 
 def get_word_count(book):
     words = book.split()
-    return len(words)
+    return len(words), words
     
+def get_chars(words):
+    chars = "".join(words).lower()
+    char_count = {}
+    for char in chars:
+        if char in char_count.keys():
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
 
 main()
