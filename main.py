63eb8ab37b93e1b3087f30814ada88a8ef9468b9
michaelboegner <cmikeb@gmail.com>
2024-01-13 15:05:01 -0500

    refactored code to be functionally oriented

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-with open("./books/frankenstein.txt") as f:
-    file_contents = f.read()
-    words = file_contents.split()
-    print(len(words))
+def main():
+    book = get_book()
+    word_count = get_word_count(book)
+    print(f"{word_count} words found in the book.")
+
+def get_book():
+    with open("./books/frankenstein.txt") as f:
+        return f.read()
+
+def get_word_count(book):
+    words = book.split()
+    return len(words)
+    
+
+main()
